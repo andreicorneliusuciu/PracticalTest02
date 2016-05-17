e1eae0089167a7135695720d4aa47161142953fc
Student User <student@cti.pub.ro>
2016-05-17 13:53:20 +0300

    GUI

--- androidstudio/PracticalTest02/app/src/main/java/practicaltest02/pdsd/systems/cs/pub/ro/practicaltest02/ServerThread.java
+++ androidstudio/PracticalTest02/app/src/main/java/practicaltest02/pdsd/systems/cs/pub/ro/practicaltest02/ServerThread.java
@@ -7,36 +7,27 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
 
-package ro.pub.cs.systems.eim.practicaltest02.network;
-
 import android.util.Log;
-
-import org.apache.http.client.ClientProtocolException;
 
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
 
-import ro.pub.cs.systems.eim.practicaltest02.general.Constants;
-import ro.pub.cs.systems.eim.practicaltest02.model.WeatherForecastInformation;
 
 public class ServerThread extends Thread {
 
     private int port = 0;
     private ServerSocket serverSocket = null;
 
-    private HashMap<String, WeatherForecastInformation> data = null;
+    private HashMap<String, String> data = null;
 
     public ServerThread(int port) {
         this.port = port;
         try {
             this.serverSocket = new ServerSocket(port);
         } catch (IOException ioException) {
-            Log.e(Constants.TAG, "An exception has occurred: " + ioException.getMessage());
-            if (Constants.DEBUG) {
-                ioException.printStackTrace();
-            }
+
         }
         this.data = new HashMap<>();
     }
